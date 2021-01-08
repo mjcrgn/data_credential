07cae8c884ee18068d8c6eea4e052d2e857c65cd
mjcrgn <76962478+mjcrgn@users.noreply.github.com>
2021-01-08 11:27:00 -0500

    .

---
+++ python files/stringSlicing.py
@@ -0,0 +1,33 @@
+weasleyFamily = [
+                 'Arthur Weasley',
+                 'Molly Weasley',
+                 'Bill Weasley',
+                 'Charles Weasley',
+                 'Percy Weasley',
+                 'Fred Weasley',
+                 'George Weasley',
+                 'Ronald Weasley',
+                 'Ginevra Weasley',
+                 'Septimus Weasley',
+                 'Cedrella Weasley',
+                 'Fleur Weasley',
+                 'Angelina Weasley',
+                 'Victoire Weasley',
+                 'Dominique Weasley',
+                 'Louis Weasley',
+                 'Lucy Weasley',
+                 'Roxanne Weasley',
+                 'Rose Granger-Weasley',
+                 'Hugo Granger-Weasley'
+]
+
+print(weasleyFamily[0:9])
+print(weasleyFamily[9:19])
+print(weasleyFamily[0:-1])
+print(weasleyFamily[:4])    # from 0 to index 4
+print(weasleyFamily[4:])    # from index 4 to -1
+print(weasleyFamily[7:11])
+print(weasleyFamily[6:9])
+print(weasleyFamily[3:8])
+print(weasleyFamily[0:0])   # prints nothing
+print(weasleyFamily[0:100]) # prints to last index
