d902d6918f4c3695887dae2e50e4d76809f1a78e
mjcrgn <76962478+mjcrgn@users.noreply.github.com>
2021-01-12 15:16:03 -0500

    210112

---
+++ files_py/210108/loop_printoddrange.py
@@ -0,0 +1,6 @@
+oddNumbers = list(range(2, 22, 2))
+for num in oddNumbers:
+  print(int(num) - 1)
+
+oddNumbers2 = list(range(1, 19, 2)) # easy way lol but make sure range starts and ends odd
+print(oddNumbers2)
---
+++ files_py/210112/coding_puzzle_01.py
@@ -0,0 +1,21 @@
+def int_addition(num):
+
+    '''
+    A function that returns the sum of all integers from 0 to input.
+    '''
+
+    # if input is not integer, function returns 0
+    if type(num) != int:
+        print('Wrong input... BYE!')
+        return 0
+
+    # otherwise, create range from 0 to num
+    # keep running total (sum) and add each element of range to sum using for loop
+    else:
+        range_list = range(num + 1)
+        sum = 0
+        for i in range_list:
+            sum = sum + i
+        return sum
+
+int_addition(10)
